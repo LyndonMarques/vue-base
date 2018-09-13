242603bbed4d38332c92efac94b5a8a3ae533f7f
Giancarlo Jung <giancarlo@pipedigital.com>
2018-09-12 23:30:22 -0300

    build 1.0.4

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,8 @@ import { create } from 'apisauce';
 import app from '../main';
 
 const api = create({
-  // baseURL: "http://127.0.0.1:8000"
-  baseURL: "http://zodiac-api.powertravel.com.br"
+  baseURL: "http://127.0.0.1:8000"
+  // baseURL: "http://zodiac-api.powertravel.com.br"
 });
 
 api.addRequestTransform(() => {
