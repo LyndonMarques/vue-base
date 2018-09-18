ee6feb1611a9ce9a894c10dd0dcb5c153e1a9633
Giancarlo Jung <giancarlo@pipedigital.com>
2018-09-18 15:07:42 -0300

    added basUrl in env files

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,7 @@ import { create } from 'apisauce';
 import app from '../main';
 
 const api = create({
-  baseURL: "http://127.0.0.1:8000"
-  // baseURL: "http://zodiac-api.powertravel.com.br"
+  baseURL: process.env.BASE_URL
 });
 
 api.addRequestTransform(() => {
